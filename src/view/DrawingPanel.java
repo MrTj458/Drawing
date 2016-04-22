bcb004166e1c26906aaeb71fb84ba3bf591e2635
Trevor <thod0127@740s-pl-130.local>
2016-04-22 13:54:31 -0600

    Added buttons for different shapes, and added methods to ShapePanel

--- src/view/DrawingPanel.java
+++ src/view/DrawingPanel.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.awt.*;
+import java.awt.event.*;
 
 import java.util.ArrayList;
 
@@ -12,16 +13,36 @@ import controller.Controller;
 
 public class DrawingPanel extends JPanel
 {
+	//Base Components
 	private Controller baseController;
 	private SpringLayout baseLayout;
+	
+	//Panels
+	private ShapePanel shapePanel;
+	
+	//Buttons
 	private JButton addRectangleButton;
+	private JButton addSquareButton;
+	private JButton addEllipseButton;
+	private JButton addCircleButton;
+	
+	//Lists
 	private ArrayList<Rectangle> rectangleList;
 	
 	public DrawingPanel(Controller baseController)
 	{
+		//Base Components
 		this.baseController = baseController;
 		this.baseLayout = new SpringLayout();
+		shapePanel = new ShapePanel();
+		
+		//Buttons
 		addRectangleButton = new JButton("Add a Rectangle");
+		addSquareButton = new JButton("Add a Square");
+		addEllipseButton = new JButton("Add a Ellipse");
+		addCircleButton = new JButton("Add a Circle");
+		
+		//Lists
 		rectangleList = new ArrayList<Rectangle>();
 		
 		setupPanel();
@@ -33,18 +54,66 @@ public class DrawingPanel extends JPanel
 	{
 		this.setLayout(baseLayout);
 		this.setBackground(Color.GRAY);
+		this.add(shapePanel);
 		this.add(addRectangleButton);
+		this.add(addSquareButton);
+		this.add(addEllipseButton);
+		this.add(addCircleButton);
 	}
 	
 	private void setupLayout()
 	{
 		baseLayout.putConstraint(SpringLayout.WEST, addRectangleButton, 10, SpringLayout.WEST, this);
 		baseLayout.putConstraint(SpringLayout.SOUTH, addRectangleButton, -10, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, shapePanel, 50, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, shapePanel, 50, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, shapePanel, -350, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, shapePanel, -50, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, addCircleButton, 0, SpringLayout.NORTH, addRectangleButton);
+		baseLayout.putConstraint(SpringLayout.WEST, addCircleButton, 6, SpringLayout.EAST, addEllipseButton);
+		baseLayout.putConstraint(SpringLayout.NORTH, addSquareButton, 0, SpringLayout.NORTH, addRectangleButton);
+		baseLayout.putConstraint(SpringLayout.WEST, addSquareButton, 6, SpringLayout.EAST, addRectangleButton);
+		baseLayout.putConstraint(SpringLayout.NORTH, addEllipseButton, 0, SpringLayout.NORTH, addRectangleButton);
+		baseLayout.putConstraint(SpringLayout.WEST, addEllipseButton, 6, SpringLayout.EAST, addSquareButton);
 	}
 	
 	private void setupListeners()
 	{
+		addRectangleButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				shapePanel.addRectangle();
+				repaint();
+			}
+		});
 		
+		addSquareButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent Click)
+			{
+				shapePanel.addSquare();
+				repaint();
+			}
+		});
+		
+		addEllipseButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent Click)
+			{
+				shapePanel.addEllipse();
+				repaint();
+			}
+		});
+		
+		addCircleButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent Click)
+			{
+				shapePanel.addCircle();
+				repaint();
+			}
+		});
 	}
 	
 	@Override
@@ -53,9 +122,9 @@ public class DrawingPanel extends JPanel
 		super.paintComponent(currentGraphics);
 		Graphics2D mainGraphics = (Graphics2D) currentGraphics;
 		
-		mainGraphics.setStroke(new BasicStroke(20));
-		mainGraphics.setColor(Color.orange);
-		mainGraphics.drawRect(50, 70, 200, 400);
+//		mainGraphics.setStroke(new BasicStroke(5));
+//		mainGraphics.setColor(Color.orange);
+//		mainGraphics.drawRect(50, 70, 200, 400);
 		
 		
 		for(Rectangle current : rectangleList)
@@ -63,9 +132,11 @@ public class DrawingPanel extends JPanel
 			int red = (int) (Math.random() * 256);
 			int green = (int) (Math.random() * 256);
 			int blue = (int) (Math.random() * 256);
+			int stroke = (int) (Math.random() * 20);
 			
+			mainGraphics.setStroke(new BasicStroke(stroke));
 			mainGraphics.setColor(new Color(red, green, blue));
-			mainGraphics.fill(current);
+			mainGraphics.draw(current);
 		}
 	}
 }
--- src/view/ShapePanel.java
+++ src/view/ShapePanel.java
@@ -1,36 +1,118 @@
 package view;
 
-import javax.swing.*;
+import java.util.ArrayList;
 
-import controller.Controller;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.*;
 
 public class ShapePanel extends JPanel
 {
-	private Controller baseController;
-	private SpringLayout baseLayout;
+	private ArrayList<Rectangle> rectangleList;
+	private ArrayList<Rectangle> squareList;
+	private ArrayList<Ellipse2D> ellipseList;
+	private ArrayList<Ellipse2D> circleList;
+	private ArrayList<Polygon> triangleList;
+	private ArrayList<Polygon> polygonList;
 	
-	public ShapePanel(Controller baseController)
+	public ShapePanel()
 	{
-		this.baseController = baseController;
-		this.baseLayout = new SpringLayout();
-		
-		setupPanel();
-		setupLayout();
-		setupListeners();
+		rectangleList = new ArrayList<Rectangle>();
+		squareList = new ArrayList<Rectangle>();
+		ellipseList = new ArrayList<Ellipse2D>();
+		circleList = new ArrayList<Ellipse2D>();
 	}
 	
-	private void setupPanel()
+	public void addRectangle()
 	{
-		this.setLayout(baseLayout);
+		int xPosition = (int) (Math.random() * getWidth());
+		int yPosition = (int) (Math.random() * getHeight());
+		int width = (int) (Math.random() * 100);
+		int height = (int) (Math.random() * 100);
+		
+		rectangleList.add(new Rectangle(xPosition, yPosition, width, height));
 	}
 	
-	private void setupLayout()
+	public void addSquare()
 	{
+		int xPosition = (int) (Math.random() * getWidth());
+		int yPosition = (int) (Math.random() * getHeight());
+		int size = (int) (Math.random() * 100);
 		
+		squareList.add(new Rectangle(xPosition, yPosition, size, size));
 	}
 	
-	private void setupListeners()
+	public void addEllipse()
 	{
+		int xPosition = (int) (Math.random() * getWidth());
+		int yPosition = (int) (Math.random() * getHeight());
+		int width = (int) (Math.random() * 100);
+		int height = (int) (Math.random() * 100);
 		
+		ellipseList.add(new Ellipse2D.Double(xPosition, yPosition, width, height));
+	}
+	
+	public void addCircle()
+	{
+		int xPosition = (int) (Math.random() * getWidth());
+		int yPosition = (int) (Math.random() * getHeight());
+		int size = (int) (Math.random() * 100);
+		
+		circleList.add(new Ellipse2D.Double(xPosition, yPosition, size, size));
+	}
+	
+	@Override
+	protected void paintComponent(Graphics currentGraphics)
+	{
+		super.paintComponent(currentGraphics);
+		Graphics2D mainGraphics = (Graphics2D) currentGraphics;
+		
+		for(Rectangle current : rectangleList)
+		{
+			int red = (int) (Math.random() * 256);
+			int green = (int) (Math.random() * 256);
+			int blue = (int) (Math.random() * 256);
+			int stroke = (int) (Math.random() * 20);
+			
+			mainGraphics.setStroke(new BasicStroke(stroke));
+			mainGraphics.setColor(new Color(red, green, blue));
+			mainGraphics.draw(current);
+		}
+		
+		for(Rectangle current : squareList)
+		{
+			int red = (int) (Math.random() * 256);
+			int green = (int) (Math.random() * 256);
+			int blue = (int) (Math.random() * 256);
+			int stroke = (int) (Math.random() * 20);
+			
+			mainGraphics.setStroke(new BasicStroke(stroke));
+			mainGraphics.setColor(new Color(red, green, blue));
+			mainGraphics.draw(current);
+		}
+		
+		for(Ellipse2D current : ellipseList)
+		{
+			int red = (int) (Math.random() * 256);
+			int green = (int) (Math.random() * 256);
+			int blue = (int) (Math.random() * 256);
+			int stroke = (int) (Math.random() * 10);
+			
+			mainGraphics.setStroke(new BasicStroke(stroke));
+			mainGraphics.setColor(new Color(red, green, blue));
+			mainGraphics.draw(current);
+		}
+		
+		for(Ellipse2D current : circleList)
+		{
+			int red = (int) (Math.random() * 256);
+			int green = (int) (Math.random() * 256);
+			int blue = (int) (Math.random() * 256);
+			int stroke = (int) (Math.random() * 10);
+			
+			mainGraphics.setStroke(new BasicStroke(stroke));
+			mainGraphics.setColor(new Color(red, green, blue));
+			mainGraphics.draw(current);
+		}
 	}
 }
